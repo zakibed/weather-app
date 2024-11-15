934b7237b54aa5f813c3381813e665f24039178d
zaki <114343341+zakibed@users.noreply.github.com>
2024-11-15 15:39:17 +0300

    Update webpack config to use ES6 modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,67 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 
-const devMode = process.env.NODE_ENV !== 'production';
+export default function (env, argv) {
+    const devMode = argv.mode === 'development';
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    devtool: devMode ? 'eval-source-map' : false,
-    devServer: {
-        hot: false
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['@babel/preset-env', { targets: 'defaults' }]
-                        ]
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: '[name].[contenthash].js',
+            path: path.resolve('./dist'),
+            clean: true
+        },
+        devtool: devMode ? 'eval-source-map' : false,
+        devServer: {
+            hot: false
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Weather App',
+                template: 'src/template.html',
+                favicon: 'src/assets/images/favicon.ico'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'assets/css/[name].[contenthash].css'
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|svg)$/i,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'assets/webfonts/[name].[hash][ext][query]'
+                    }
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            targets: '> 0.25%, not dead',
+                            presets: [['@babel/preset-env']],
+                            cacheDirectory: true
+                        }
                     }
                 }
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|svg)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/webfonts/[name].[hash][ext][query]'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Weather App',
-            template: 'src/template.html',
-            favicon: 'src/assets/images/favicon.ico'
-        })
-    ].concat(
-        devMode
-            ? []
-            : [
-                  new MiniCssExtractPlugin({
-                      filename: 'assets/css/[name].[contenthash].css'
-                  })
-              ]
-    ),
-    optimization: {
-        minimizer: ['...', new CssMinimizerPlugin()]
-    },
-    performance: {
-        maxAssetSize: 500000
-    }
-};
+            ]
+        },
+        optimization: {
+            minimizer: ['...', new CssMinimizerPlugin()]
+        },
+        performance: {
+            maxAssetSize: 500000
+        }
+    };
+}
