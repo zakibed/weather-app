21706c356387938ec343f110df2a72b76aab0ad3
zaki <114343341+zakibed@users.noreply.github.com>
2024-11-15 14:45:31 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        hot: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/webfonts/[name].[hash][ext][query]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Weather App',
+            template: 'src/template.html'
+        })
+    ].concat(
+        devMode
+            ? []
+            : [
+                  new MiniCssExtractPlugin({
+                      filename: 'assets/css/[name].[contenthash].css'
+                  })
+              ]
+    ),
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()]
+    }
+};
